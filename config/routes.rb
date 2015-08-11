d047f5ad280bd239dd77cf2450929646aaf467e1
Amanda Lemmons <amandalemmons@Amandas-MBP.home>
2015-08-11 17:47:25 -0500

    add user profil

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+
   devise_for :users, controllers: {registrations: 'registrations'}
   root 'pages#home'
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+
+  resources :users, only: [:show]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
